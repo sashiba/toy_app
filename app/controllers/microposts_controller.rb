6447034c97f37089a33d1b1f7d2e9ce15c450244
Alexander Pirnarev <sashopirnarev@gmail.com>
2016-02-29 16:43:57 +0200

    added methods in controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,40 @@
 class MicropostsController < ApplicationController
+  def index
+    @microposts = Micropost.all
+  end
+
+  def show
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def new
+    @micropost = Micropost.new
+  end
+
+  def create
+    @micropost = Micropost.create!(micropost_params)
+    redirect_to microposts_path
+  end
+
+  def edit
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def update
+    @micropost = Micropost.find(params[:id])
+    @micropost.update!(micropost_params)
+    redirect_to micropost_path(@micropost)
+  end
+
+  def destroy 
+    @micropost = Micropost.find(params[:id])
+    @micropost.destroy
+    redirect_to micropost_path
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :user_id)
+  end
 end
